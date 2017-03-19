52b87f0ad0ffed7a29c4aa90f7a40c38f06e335d
mattsoave <mattsoave@gmail.com>
2017-03-19 12:45:43 -0700

    Added CSS

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ const gulp = require('gulp');
 const babel = require('gulp-babel');
 const rename = require ('gulp-rename');
 const uglify = require('gulp-uglify');
+const sass = require('gulp-sass');
 
 const JS_SRC = "./js/src";
 const JS_DEST = "./js/build";
@@ -16,4 +17,13 @@ gulp.task("js", function() {
         .pipe(uglify())
         .pipe(rename({ extname: ".min.js"}))
         .pipe(gulp.dest(JS_DEST));
+});
+
+
+
+
+gulp.task('css', function () {
+  return gulp.src('./css/src/*.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('./css/build'));
 });
